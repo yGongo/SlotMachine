67574435a3a685d19878f8925a078eeb7c1cd149
Eugenio Malenchi <114256839+yGongo@users.noreply.github.com>
2023-12-20 09:53:09 +0100

    Add files via upload

---
+++ Malenchi.Eugenio.4i.CliSlotMachine/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Malenchi.Eugenio._4i.LibSlotMachine;
+
+namespace Malenchi.Eugenio._4i.CliSlotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Book Of Rah!");
+            SlotMachine s = new SlotMachine(10);
+            Console.WriteLine(s.Credito);
+        }
+    }
+}
