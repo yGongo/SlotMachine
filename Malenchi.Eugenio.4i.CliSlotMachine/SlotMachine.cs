fc6685c35ceae35ea5cec458f8cfc3a9e09dba3b
Eugenio Malenchi <114256839+yGongo@users.noreply.github.com>
2024-01-10 10:56:39 +0100

    Add files via upload

---
+++ Malenchi.Eugenio.4i.CliSlotMachine/SlotMachine.cs
@@ -0,0 +1,142 @@
+﻿using Eugenio.Malenchi._4i.LibSlotMachine;
+
+namespace Malenchi.Eugenio._4i.CliSlotMachine
+{
+    internal partial class Program
+    {
+        SlotMachine slotMachine = new();
+        int contatore1 = 0;
+        int contatore2 = 0;
+        int contatore3 = 0;
+        bool blocca1 = false;
+        bool blocca3 = false;
+        bool blocca2 = false;
+
+        private static void Main()
+        {
+            Program programma = new();
+            programma.MainProgramma();
+        }
+
+        private void MainProgramma()
+        {
+            bool menu = true;
+
+            Console.WriteLine(("SlotMachine"));
+
+            while (menu)
+            {
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine(($"\t {slotMachine.Ruota1} | {slotMachine.Ruota2} | {slotMachine.Ruota3}"));
+                Console.ResetColor();
+
+                Console.WriteLine($"\t Credito rimasto {slotMachine.Saldo}");
+                Console.WriteLine($"\t Ultima vincita {slotMachine.UltimaVincita}");
+                Console.WriteLine();
+
+                Console.WriteLine("1- Gira la Ruota");
+
+                Console.WriteLine("2- Blocca la prima lettera");
+                Console.WriteLine("3- Blocca la seconda lettera");
+                Console.WriteLine("4- Blocca la terza lettera");
+                Console.WriteLine();
+
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case '1':
+                        try
+                        {
+                            slotMachine.GiraSlotMachine(blocca1, blocca2, blocca3);
+                            Verifica();
+                            Console.Clear();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine(e.Message);
+                            Console.WriteLine("Vuoi ricominciare da capo? (s / n)");
+                            switch (Console.ReadKey().KeyChar)
+                            {
+                                case 's':
+                                    slotMachine = new SlotMachine();
+                                    break;
+
+                                case 'n':
+                                    Console.Clear();
+                                    Console.WriteLine(("Arrivederci"));
+                                    Console.ReadKey();
+                                    menu = false;
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Devi scegliere tra s (si) e n (no)");
+                                    break;
+                            }
+                        }
+                        break;
+
+                    case '2':
+                        if (blocca1 == false && !(blocca2 == true && blocca3 == true))
+                            blocca1 = true;
+                        else
+                            blocca1 = false;
+
+                        Console.WriteLine(" - Hai bloccato la prima lettera per 2 turni");
+                        break;
+
+                    case '3':
+                        if (blocca2 == false && !(blocca1 == true && blocca3 == true))
+                            blocca2 = true;
+                        else
+                            blocca2 = false;
+
+                        Console.WriteLine(" - Hai bloccato la seconda lettera per 2 turni");
+                        break;
+
+                    case '4':
+                        if (blocca3 == false && !(blocca1 == true && blocca2 == true))
+                            blocca3 = true;
+                        else
+                            blocca3 = false;
+
+                        Console.WriteLine(" - Hai bloccato la terza lettera per 2 turni");
+                        break;
+
+                }
+            }
+        }
+
+        public void Verifica()
+        {
+            if (blocca1 == true)
+                contatore1++;
+
+            if (contatore1 == 2)
+            {
+                blocca1 = false;
+                contatore1 = 0;
+            }
+
+            if (blocca2 == true)
+                contatore2++;
+
+            if (contatore2 == 2)
+            {
+                blocca2 = false;
+                contatore2 = 0;
+            }
+
+            if (blocca3 == true)
+                contatore3++;
+
+            if (contatore3 == 2)
+            {
+                blocca3 = false;
+                contatore3 = 0;
+            }
+        }
+    }
+}
---
+++ Malenchi.Eugenio.4i.LibSlotMachine/SlotMachine.cs
@@ -0,0 +1,110 @@
+﻿namespace Eugenio.Malenchi._4i.LibSlotMachine
+{
+    public class SlotMachine
+    {
+        public int Saldo { get; set; }
+        public int UltimaVincita { get; set; }
+        public char Ruota1 { get; set; }
+        public char Ruota2 { get; set; }
+        public char Ruota3 { get; set; }
+
+        private const int JACKPOT = 100;
+        private const int VITTORIA_DIFFICILE = 50;
+
+        public SlotMachine()
+        {
+            Saldo = 10;
+            UltimaVincita = 0;
+            Ruota1 = Ruota2 = Ruota3 = '-';
+        }
+
+        public void GiraSlotMachine()
+        {
+            if (Saldo == 0)
+                throw new Exception("Hai finito il saldo a disposizione");
+
+            Saldo--;
+
+            Random r = new Random();
+
+            Ruota1 = (char)r.Next(65, 91);
+            Ruota2 = (char)r.Next(65, 91);
+            Ruota3 = (char)r.Next(65, 91);
+
+            if (JackPot())
+                AggiungiVincita(JACKPOT);
+
+            else if (VittoriaDifficile())
+                AggiungiVincita(VITTORIA_DIFFICILE);
+
+            else if (VittoriaSemplice())
+                AggiungiVincita((int)Ruota1 - 64);
+
+            else if (NessunaPerdita())
+                AggiungiVincita(1);
+
+            else
+                UltimaVincita = 0;
+        }
+
+        public void GiraSlotMachine(bool blocca1, bool blocca2, bool blocca3)
+        {
+            if (Saldo == 0)
+                throw new Exception("Hai finito il saldo a disposizione");
+
+            Saldo--;
+
+            Random r = new Random();
+
+            if (!blocca1)
+                Ruota1 = (char)r.Next(65, 91);
+
+            if (!blocca2)
+                Ruota2 = (char)r.Next(65, 91);
+
+            if (!blocca3)
+                Ruota3 = (char)r.Next(65, 91);
+
+            if (JackPot())
+                AggiungiVincita(JACKPOT);
+
+            else if (VittoriaDifficile())
+                AggiungiVincita(VITTORIA_DIFFICILE);
+
+            else if (VittoriaSemplice())
+                AggiungiVincita((int)Ruota1 - 64);
+
+            else if (NessunaPerdita())
+                AggiungiVincita(1);
+
+            else
+                UltimaVincita = 0;
+        }
+
+        private bool JackPot()
+        {
+            return (Ruota1 == 'Z' && Ruota2 == 'Z' && Ruota3 == 'Z');
+        }
+
+        private bool VittoriaDifficile()
+        {
+            return (Ruota2 == Ruota1 + 1 && Ruota3 == Ruota2 + 1);
+        }
+
+        private bool VittoriaSemplice()
+        {
+            return (Ruota1 == Ruota2 && Ruota2 == Ruota3);
+        }
+
+        private bool NessunaPerdita()
+        {
+            return (Ruota1 == Ruota2 || Ruota2 == Ruota3 || Ruota3 == Ruota1);
+        }
+
+        private void AggiungiVincita(int vincita)
+        {
+            Saldo += vincita;
+            UltimaVincita = vincita;
+        }
+    }
+}
